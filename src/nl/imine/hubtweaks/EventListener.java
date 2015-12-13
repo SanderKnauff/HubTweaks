14168ede11379426c4adc4254b59d55f0c43e4c8
MakerTim <makertim@outlook.com>
2015-12-13 15:40:36 +0100

    Changed move event to a runnable so all entetys can be checked

--- src/nl/imine/hubtweaks/EventListener.java
+++ src/nl/imine/hubtweaks/EventListener.java
@@ -8,61 +8,77 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class EventListener implements Listener {
+public class EventListener implements Listener, Runnable {
 
     public static void init() {
-        HubTweaks.getInstance().getServer().getPluginManager().registerEvents(new EventListener(), HubTweaks.getInstance());
+        EventListener el = new EventListener();
+        HubTweaks.getInstance().getServer().getPluginManager().registerEvents(el, HubTweaks.getInstance());
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(HubTweaks.getInstance(), el, 1L, 1L);
     }
 
-    @EventHandler
-    public void onPlayerMove(final PlayerMoveEvent evt) {
-        if (evt.getPlayer().getLocation().getBlockY() < 0) {
-            evt.getPlayer().teleport(evt.getPlayer().getWorld().getSpawnLocation());
-            playerRespawn(evt.getPlayer());
+    public void run() {
+        for (World w : Bukkit.getWorlds()) {
+            for (Entity e : w.getEntities()) {
+                if (e instanceof LivingEntity) {
+                    teleportSpawn(e);
+                }
+            }
+        }
+    }
+
+    private void teleportSpawn(final Entity e) {
+        if (e.getLocation().getY() <= 0) {
+            e.setFallDistance(0f);
+            e.teleport(e.getWorld().getSpawnLocation());
+            if (e instanceof Player) {
+                playerRespawn((Player) e);
+            }
         }
     }
 
     @EventHandler
-    public void onPlayerDisconnect(final PlayerQuitEvent Event) {
+    public void onPlayerDisconnect(final PlayerQuitEvent pqe) {
         Bukkit.getScheduler().scheduleSyncDelayedTask(HubTweaks.getInstance(), () -> {
-            PlayerDataManager.RemovePlayerData(Event.getPlayer());
-        }, 10);
+            PlayerDataManager.RemovePlayerData(pqe.getPlayer());
+        } , 10);
     }
 
     @EventHandler
-    public void onPlayerItemDrop(final PlayerDropItemEvent evt) {
-        if (!evt.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
-            evt.setCancelled(true);
+    public void onPlayerItemDrop(final PlayerDropItemEvent pdie) {
+        if (!pdie.getPlayer().getGameMode().equals(GameMode.CREATIVE)) {
+            pdie.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerJoin(final PlayerJoinEvent evt) {
-        this.playerRespawn(evt.getPlayer());
+    public void onPlayerJoin(final PlayerJoinEvent pje) {
+        this.playerRespawn(pje.getPlayer());
     }
 
     @EventHandler
-    public void onPlayerRespawn(final PlayerRespawnEvent evt) {
-        this.playerRespawn(evt.getPlayer());
+    public void onPlayerRespawn(final PlayerRespawnEvent pre) {
+        this.playerRespawn(pre.getPlayer());
     }
 
-    private void playerRespawn(final Player player) {
+    private void playerRespawn(final Player pl) {
         FileConfiguration config = HubTweaks.getInstance().getConfig();
-        player.setGameMode(GameMode.ADVENTURE);
-        Parkour.getInstance().getPlayer(player).setTouchedPlate(false);
+        pl.setGameMode(GameMode.ADVENTURE);
+        Parkour.getInstance().getPlayer(pl).setTouchedPlate(false);
         final ItemStack item = new ItemStack(Material.COMPASS, 1);
         ItemMeta metadat = (ItemMeta) item.getItemMeta();
         List<String> list = new ArrayList<>();
@@ -70,24 +86,25 @@ public class EventListener implements Listener {
         metadat.setLore(list);
         metadat.setDisplayName(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD.toString() + "Teleporter");
         item.setItemMeta(metadat);
-        player.closeInventory();
+        pl.closeInventory();
         Bukkit.getScheduler().scheduleSyncDelayedTask(HubTweaks.getInstance(), new Runnable() {
             @Override
             public void run() {
-                player.getInventory().clear();
-                player.getInventory().addItem(item);
+                pl.getInventory().clear();
+                pl.getInventory().addItem(item);
                 if (config.getConfigurationSection("RuleBook.Pages") != null) {
                     if (!config.getConfigurationSection("RuleBook.Pages").getKeys(false).isEmpty()) {
                         ItemStack RuleBook = new ItemStack(Material.WRITTEN_BOOK, 1);
                         BookMeta RuleBookMeta = (BookMeta) RuleBook.getItemMeta();
                         List<String> PageList = new ArrayList<>();
-                        for (int Pages = 1; Pages <= config.getConfigurationSection("RuleBook.Pages").getKeys(false).size(); Pages++) {
+                        for (int Pages = 1; Pages <= config.getConfigurationSection("RuleBook.Pages").getKeys(false)
+                                .size(); Pages++) {
                             PageList.add(config.getString("RuleBook.Pages." + Pages));
                         }
                         RuleBookMeta.setPages(PageList);
                         RuleBookMeta.setTitle(config.getString("RuleBook.Title"));
                         RuleBook.setItemMeta(RuleBookMeta);
-                        player.getInventory().addItem(RuleBook);
+                        pl.getInventory().addItem(RuleBook);
                     }
                 }
             }
