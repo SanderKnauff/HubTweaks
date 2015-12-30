26597d52f12175f8ee24fbec94a705add8586d4f
Sansko1337 <sansko1337@users.noreply.github.com>
2015-12-30 14:19:40 +0100

    Added a new menu for joining UHC Servers

--- src/nl/imine/hubtweaks/warps/CompassWarp.java
+++ src/nl/imine/hubtweaks/warps/CompassWarp.java
@@ -29,34 +29,36 @@ public class CompassWarp implements Listener {
     }
 
     public CompassWarp() {
-        inv = GuiManager.getInstance().createContainer(ChatColor.DARK_GRAY + "Select your destination:", 18, false,
-                false);
+        Bukkit.getScheduler().runTaskAsynchronously(HubTweaks.getInstance(), () -> {
+            inv = GuiManager.getInstance().createContainer(ChatColor.DARK_GRAY + "Select your destination:", 18, false,
+                    false);
 
-        inv.addButton(new ButtonTeleport(inv,
-                ItemUtil.getBuilder(Material.INK_SACK).setDurability((short) 5)
-                        .setName(ChatColor.LIGHT_PURPLE + "Spawn").build(),
-                0, new Location(HubTweaks.getMainWorld(), 52.5D, 36.1D, -503.5D, -90F, 0F)));
-        inv.addButton(
-                new ButtonTeleport(inv, ItemUtil.getBuilder(Material.TNT).setName(ChatColor.YELLOW + "Outlaws").build(),
-                        2, new Location(HubTweaks.getMainWorld(), 73.5D, 36.1D, -503.5D, -90F, 0F)));
-        inv.addButton(new ButtonTeleport(
-                inv, ItemUtil.getBuilder(Material.GOLDEN_APPLE).setDurability((short) 1)
-                        .setName(ChatColor.DARK_RED + "UHC").build(),
-                4, new Location(HubTweaks.getMainWorld(), 53.5D, 33.1D, -482.5D, 0F, 0F)));
-        inv.addButton(new ButtonTeleport(inv,
-                ItemUtil.getBuilder(Material.GOLD_PICKAXE).setName(ChatColor.GOLD + "Survival")
-                        .addFlag(ItemFlag.HIDE_ATTRIBUTES).build(),
-                6, new Location(HubTweaks.getMainWorld(), 28.5D, 31.1D, -500.5D, 90F, 0F)));
-        inv.addButton(new ButtonTeleport(inv,
-                ItemUtil.getBuilder(Material.BRICK).setName(ChatColor.GREEN + "Creative").build(), 8,
-                new Location(HubTweaks.getMainWorld(), 33.5D, 64.1D, -534.5D, 180F, 0F)));
+            inv.addButton(new ButtonTeleport(inv,
+                    ItemUtil.getBuilder(Material.INK_SACK).setDurability((short) 5)
+                    .setName(ChatColor.LIGHT_PURPLE + "Spawn").build(),
+                    0, new Location(HubTweaks.getMainWorld(), 52.5D, 36.1D, -503.5D, -90F, 0F)));
+            inv.addButton(
+                    new ButtonTeleport(inv, ItemUtil.getBuilder(Material.TNT).setName(ChatColor.YELLOW + "Outlaws").build(),
+                            2, new Location(HubTweaks.getMainWorld(), 73.5D, 36.1D, -503.5D, -90F, 0F)));
+            inv.addButton(new ButtonBrowseUHC(
+                    inv, ItemUtil.getBuilder(Material.GOLDEN_APPLE).setDurability((short) 1)
+                    .setName(ChatColor.DARK_RED + "UHC").build(),
+                    4));
+            inv.addButton(new ButtonTeleport(inv,
+                    ItemUtil.getBuilder(Material.GOLD_PICKAXE).setName(ChatColor.GOLD + "Survival")
+                    .addFlag(ItemFlag.HIDE_ATTRIBUTES).build(),
+                    6, new Location(HubTweaks.getMainWorld(), 28.5D, 31.1D, -500.5D, 90F, 0F)));
+            inv.addButton(new ButtonTeleport(inv,
+                    ItemUtil.getBuilder(Material.BRICK).setName(ChatColor.GREEN + "Creative").build(), 8,
+                    new Location(HubTweaks.getMainWorld(), 33.5D, 64.1D, -534.5D, 180F, 0F)));
 
-        inv.addButton(new ButtonOitc(inv, 12));
-        inv.addButton(
-                new ButtonTeleport(inv, ItemUtil.getBuilder(Material.STICK).setName(ChatColor.AQUA + "Tag").build(), 14,
-                        new Location(HubTweaks.getMainWorld(), -15.5D, 33.1D, -481.5D, -90F, 0F)));
+            inv.addButton(new ButtonOitc(inv, 12));
+            inv.addButton(
+                    new ButtonTeleport(inv, ItemUtil.getBuilder(Material.STICK).setName(ChatColor.AQUA + "Tag").build(), 14,
+                            new Location(HubTweaks.getMainWorld(), -15.5D, 33.1D, -481.5D, -90F, 0F)));
 
-        Bukkit.getPluginManager().registerEvents(this, HubTweaks.getInstance());
+            Bukkit.getPluginManager().registerEvents(this, HubTweaks.getInstance());
+        });
     }
 
     @EventHandler
