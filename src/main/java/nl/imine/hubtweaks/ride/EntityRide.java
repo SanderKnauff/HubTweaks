a01dd08aeba068c988ed94966a90397ce0c96452
MakerTim <makertim@outlook.com>
2016-01-13 00:47:42 +0100

    undo

--- src/main/java/nl/imine/hubtweaks/ride/EntityRide.java
+++ src/main/java/nl/imine/hubtweaks/ride/EntityRide.java
@@ -45,7 +45,6 @@ public class EntityRide implements Listener {
         if (ptse.isSneaking()) {
             addToTimeout(pl);
             pl.eject();
-            pl.teleport(pl.getLocation().add(0D, -0.5D, 0D));
         }
     }
 
