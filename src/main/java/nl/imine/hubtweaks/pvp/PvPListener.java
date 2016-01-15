d5cc23e835e91fb91939c23616cb22160cea8398
Sansko1337 <sansko1337@users.noreply.github.com>
2016-01-16 00:24:32 +0100

    Debug the interact event

--- src/main/java/nl/imine/hubtweaks/pvp/PvPListener.java
+++ src/main/java/nl/imine/hubtweaks/pvp/PvPListener.java
@@ -166,7 +166,6 @@ public class PvPListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent evt) {
         if (PvP.getPlayerList().contains(evt.getPlayer())) {
             evt.getPlayer().getInventory().addItem(new ItemStack(Material.ARROW, 1));
-            evt.getPlayer().getInventory().removeItem(new ItemStack(Material.ARROW, 1));
         }
     }
 }
