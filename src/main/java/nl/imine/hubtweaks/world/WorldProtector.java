5b3ad2dce2b2a8bdb0411a3555bc251b736b4ba7
MakerTim <makertim@outlook.com>
2016-01-15 02:39:00 +0100

    itemframes are safe!

--- src/main/java/nl/imine/hubtweaks/world/WorldProtector.java
+++ src/main/java/nl/imine/hubtweaks/world/WorldProtector.java
@@ -3,6 +3,7 @@ package nl.imine.hubtweaks.world;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,6 +12,7 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
@@ -37,6 +39,14 @@ public class WorldProtector implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
+    public void onHangBreak(HangingBreakByEntityEvent hbbee) {
+        if (hbbee.getRemover() instanceof Player && ((Player) hbbee.getRemover()).getGameMode() == GameMode.ADVENTURE
+                || hbbee instanceof Projectile) {
+            hbbee.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntity(EntityDamageEvent ede) {
         if (ede.getEntity() instanceof Player) {
             switch (ede.getCause()) {
@@ -59,10 +69,10 @@ public class WorldProtector implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onEntityDamageByEntity(EntityExplodeEvent  eee) {
+    public void onEntityDamageByEntity(EntityExplodeEvent eee) {
         eee.setCancelled(true);
     }
-    
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent edbee) {
         if (edbee.getEntity() instanceof Player) {
