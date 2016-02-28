b1ecfff7f7642b3e00e10ab7224988ea6e3265ef
MakerTim <makertim@outlook.com>
2016-02-28 16:10:45 +0100

    Concept 2.0 flydetection -> if no blocks arround = fly

--- src/main/java/nl/imine/hubtweaks/world/AntiFly.java
+++ src/main/java/nl/imine/hubtweaks/world/AntiFly.java
@@ -6,55 +6,55 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.entity.Player;
 
 import nl.imine.api.util.ColorUtil;
 import nl.imine.api.util.PlayerUtil;
 import nl.imine.hubtweaks.HubTweaks;
 
-public class AntiFly implements Listener {
+public class AntiFly {
 
     private Map<UUID, Integer> flyMap = new HashMap<>();
-    private boolean scan = false;
 
     public static void init() {
         new AntiFly();
     }
 
     public AntiFly() {
-        Bukkit.getPluginManager().registerEvents(this, HubTweaks.getInstance());
         Bukkit.getScheduler().scheduleSyncRepeatingTask(HubTweaks.getInstance(), () -> {
+            flyMap.entrySet().stream().filter(map -> Bukkit.getPlayer(map.getKey()) != null).forEach(map -> {
+                if (map.getValue() > 15) {
+                    Player pl = Bukkit.getPlayer(map.getKey());
+                    PlayerUtil.sendGlobalAdmin(ColorUtil.replaceColors(
+                            "&l[&5FLY LOG&r&l]&r &c%s &7is now flying in &e%s&7. [&ePackets: &c%d&7]", pl.getName(),
+                            pl.getWorld().getName(), map.getValue()));
+                }
+            });
             flyMap.clear();
-            scan = true;
         } , 0L, 20L * 3);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(HubTweaks.getInstance(), () -> {
+            Bukkit.getOnlinePlayers().stream().filter(pl -> isFlying(pl)).forEach(pl -> addFly(pl));
+        } , 0L, 20L / 4);
     }
 
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent pme) {
-        if (pme.getFrom().getY() < pme.getTo().getY() && !pme.getPlayer().getAllowFlight() && scan) {
-            for (int x = -1; x < 2; x++) {
-                for (int y = -1; y < 1; y++) {
-                    for (int z = -1; z < 2; z++) {
-                        if (pme.getFrom().clone().add(x, y, z).getBlock().getType() != Material.AIR) {
-                            return;
-                        }
+    private void addFly(Player pl) {
+        if (!flyMap.containsKey(pl.getUniqueId())) {
+            flyMap.put(pl.getUniqueId(), 0);
+        }
+        flyMap.put(pl.getUniqueId(), flyMap.get(pl.getUniqueId()) + 1);
+    }
+
+    private static boolean isFlying(Player pl) {
+        for (int x = -1; x < 2; x++) {
+            for (int y = -1; y < 2; y++) {
+                for (int z = -1; z < 2; z++) {
+                    if (pl.getLocation().clone().add(x, y, z).getBlock().getType() != Material.AIR) {
+                        return false;
                     }
                 }
             }
-            UUID uuid = pme.getPlayer().getUniqueId();
-            if (!flyMap.containsKey(uuid)) {
-                flyMap.put(uuid, 0);
-            }
-            flyMap.put(uuid, flyMap.get(uuid) + 1);
-            if (flyMap.get(uuid) > 20) {
-                PlayerUtil
-                        .sendGlobalAdmin(ColorUtil.replaceColors("&l[&5FLY LOG&r&l]&r &c%s &7is now flying in &e%s&7. [&c%d&7]",
-                                pme.getPlayer().getName(), pme.getFrom().getWorld().getName(), flyMap.get(uuid)));
-            }
-        } else if (flyMap.containsKey(pme.getPlayer().getUniqueId())) {
-            flyMap.put(pme.getPlayer().getUniqueId(), 0);
         }
+        return true;
     }
+
 }
