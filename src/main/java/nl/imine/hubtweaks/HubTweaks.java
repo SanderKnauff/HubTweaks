ccd41e9fdcaa53d7669efdded389e59c7f4ddf06
MakerTim <makertim@outlook.com>
2016-02-28 15:12:07 +0100

    Concept flylog in adminchat

--- src/main/java/nl/imine/hubtweaks/HubTweaks.java
+++ src/main/java/nl/imine/hubtweaks/HubTweaks.java
@@ -12,6 +12,7 @@ import nl.imine.hubtweaks.parkour.Parkour;
 import nl.imine.hubtweaks.pvp.PvP;
 import nl.imine.hubtweaks.ride.EntityRide;
 import nl.imine.hubtweaks.warps.CompassWarp;
+import nl.imine.hubtweaks.world.AntiFly;
 import nl.imine.hubtweaks.world.WorldProtector;
 
 public class HubTweaks extends JavaPlugin {
@@ -30,6 +31,7 @@ public class HubTweaks extends JavaPlugin {
         Parkour.init();
         EntityRide.init();
         Spawner.init();
+        AntiFly.init();
         Statistic.init();
         Bukkit.getMessenger().registerOutgoingPluginChannel(plugin, "BungeeCord");
         getCommand("HubTweaks").setExecutor(new CommandHandler(this));
---
+++ src/main/java/nl/imine/hubtweaks/world/AntiFly.java
@@ -0,0 +1,48 @@
+package nl.imine.hubtweaks.world;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import nl.imine.api.util.ColorUtil;
+import nl.imine.api.util.PlayerUtil;
+import nl.imine.hubtweaks.HubTweaks;
+
+public class AntiFly implements Listener {
+
+    private Map<UUID, Integer> flyMap = new HashMap<>();
+
+    public static void init() {
+        new AntiFly();
+    }
+
+    public AntiFly() {
+        Bukkit.getPluginManager().registerEvents(this, HubTweaks.getInstance());
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(HubTweaks.getInstance(), () -> {
+            flyMap.clear();
+        } , 0L, 20L);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent pme) {
+        if (pme.getFrom().getY() > pme.getTo().getY()
+                && pme.getFrom().add(0, -1, 0).getBlock().getType() != Material.AIR) {
+            UUID uuid = pme.getPlayer().getUniqueId();
+            if (!flyMap.containsKey(uuid)) {
+                flyMap.put(uuid, 0);
+            }
+            flyMap.put(uuid, flyMap.get(uuid) + 1);
+            if (flyMap.get(uuid) > 10) {
+                PlayerUtil
+                        .sendGlobalAdmin(ColorUtil.replaceColors("&l[&5FLY LOG&r&l]&r &c%s &7is now flying in &e%s&7.",
+                                pme.getPlayer().getName(), pme.getFrom().getWorld().getName()));
+            }
+        }
+    }
+}
