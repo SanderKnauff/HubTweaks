e5d09a20de2022ff0bbbd3a22cd9da052f9ba3c7
Sansko1337 <sansko1337@users.noreply.github.com>
2016-03-14 19:56:46 +0100

    Added debugging for parkourLevelSorting

--- src/main/java/nl/imine/hubtweaks/parkour/ParkourManager.java
+++ src/main/java/nl/imine/hubtweaks/parkour/ParkourManager.java
@@ -79,6 +79,7 @@ public class ParkourManager implements Listener {
 				// Create new timings.
 				ParkourLevel lastLevel = parkour.getLevels().stream().filter(l -> !l.isBonusLevel())
 						.sorted((ParkourLevel p1, ParkourLevel p2) -> p1.getLevel() - p2.getLevel()).findFirst().get();
+				System.out.println("Last Level: " + lastLevel.getLevel());
 				// Time between levels
 				if (goal.getLevel().getLevel() < lastLevel.getLevel()) {
 					player.addPendingTime(new ParkourTiming(null, goal.getLevel(),
