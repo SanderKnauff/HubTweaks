7ca9dd8091fa71115154f2900757ae8199033471
root <root@imine.nl>
2016-03-21 15:38:48 +0100

    Debug dependency

--- src/main/java/nl/imine/hubtweaks/parkour/ParkourAntiCheat.java
+++ src/main/java/nl/imine/hubtweaks/parkour/ParkourAntiCheat.java
@@ -11,6 +11,7 @@ import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.Material;
 
 public class ParkourAntiCheat implements Listener {
 
@@ -20,6 +21,7 @@ public class ParkourAntiCheat implements Listener {
 		player.setCheated(true);
 		player.setLastLevel(ParkourManager.getParkourInstance().getLevels().stream().filter(l -> !l.isBonusLevel())
 				.sorted((ParkourLevel p1, ParkourLevel p2) -> p2.getLevel() - p1.getLevel()).findFirst().get());
+		System.out.println(Material.FROSTED_ICE);
 	}
 
 	@EventHandler
