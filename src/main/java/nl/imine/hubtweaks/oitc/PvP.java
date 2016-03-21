362f4d46ed7bee6f74d49a4246ef5414168167b9
Sansko1337 <sansko1337@users.noreply.github.com>
2016-03-21 18:42:06 +0100

    Debug pvp

--- src/main/java/nl/imine/hubtweaks/oitc/PvP.java
+++ src/main/java/nl/imine/hubtweaks/oitc/PvP.java
@@ -10,12 +10,12 @@ import nl.imine.hubtweaks.HubTweaks;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
@@ -30,9 +30,10 @@ public class PvP {
 	public static void init() {
 		PvPListener.init();
 		loadArena();
-	}
-
-	public PvP(Plugin plugin) {
+		Bukkit.getScheduler().runTaskTimer(HubTweaks.getInstance(), () -> {
+			LocationUtil.getBlocksInOutline(BOX[0], BOX[1]).stream()
+					.forEach(l -> Bukkit.getWorlds().get(0).spawnParticle(Particle.REDSTONE, l.getLocation(), 0));
+		} , 5, 5l);
 	}
 
 	public static boolean isPlayerInArena(Player player) {
