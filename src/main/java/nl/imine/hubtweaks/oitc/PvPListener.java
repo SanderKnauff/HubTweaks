826046c9f655bf799db5086b3c4e668f4136b299
Sansko1337 <sanderknauff@hotmail.com>
2016-05-08 02:07:28 +0200

    Ban projectiles from hitting itemframes

--- src/main/java/nl/imine/hubtweaks/oitc/PvPListener.java
+++ src/main/java/nl/imine/hubtweaks/oitc/PvPListener.java
@@ -12,6 +12,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -81,6 +82,11 @@ public class PvPListener implements Listener {
 					}
 					evt.setCancelled(true);
 				}
+			}
+		}
+		if (evt.getEntity().getType().equals(EntityType.ITEM_FRAME)) {
+			if (evt.getDamager() instanceof Projectile) {
+				evt.setCancelled(true);
 			}
 		}
 	}
